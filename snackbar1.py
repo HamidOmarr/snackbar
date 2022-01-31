4c49759fcaa2517070082055fdcfc3ee84a48fed
HamidOmarr <99067332@mydavinci.nl>
2022-01-31 10:00:26 +0100

    next

---
+++ snackbar1.py
@@ -0,0 +1,15 @@
+TotaalPatat = int(input("Hoeveel Patat?"))
+TotaalFrikadellen = int(input("Hoeveel Frikadellen?"))
+TotaalKroketten = int(input("Hoeveel Kroketten?"))
+
+patat = 1.50
+frikadellen = 1.80
+kroketten = 2.00
+
+if TotaalPatat > 0:
+    print( "Patat:" , TotaalPatat)
+if TotaalFrikadellen > 0:
+    print("Frikadellen:" ,TotaalFrikadellen) 
+if TotaalKroketten > 0:
+    print("Kroketten:" , TotaalKroketten)
+  
